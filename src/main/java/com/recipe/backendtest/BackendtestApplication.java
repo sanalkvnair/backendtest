6388bc9345cc90951eed5183966cf9316f33b1f0
Sanal Nair <sanalkvnair@gmail.com>
2018-10-19 16:07:52 +0200

    Initial code setup commit

---
+++ src/main/java/com/recipe/backendtest/BackendtestApplication.java
@@ -0,0 +1,12 @@
+package com.recipe.backendtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendtestApplication.class, args);
+	}
+}
